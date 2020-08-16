74f3977aa69bc61bdfaebc124294433639e0fbd7
SwiftyPython <61612668+SwiftyPython@users.noreply.github.com>
2020-08-17 01:25:06 +0200

    Add files via upload

---
+++ i am poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  i am poor
+//
+//  Created by jason Moses on 2018/09/30.
+//  Copyright © 2018 jason Moses. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
